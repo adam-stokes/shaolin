e2347b4bc881ccbc52818fe451c1d7431f9e8cf8
Adam Stokes <battlemidget@users.noreply.github.com>
2020-01-08 14:55:43 -0500

    some arg work
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/CI/Repo.pm
+++ lib/CI/Repo.pm
@@ -7,9 +7,9 @@ use Data::Dumper;
 has layer_list => (is => 'ro');
 has charm_list => (is => 'ro');
 
-sub init {
+sub sync_upstream {
     my ($self, $args) = @_;
-    print Dumper($self);
+    print "Syncing upstream\n";
 }
 
 1;
